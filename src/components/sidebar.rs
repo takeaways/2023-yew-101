276ce774819d806a610b1f277f699bc72f35ae68
takeaways <wkdrjsdlf2@gmail.com>
2023-08-28 22:18:31 +0900

    중복제거

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -2,49 +2,33 @@ use crate::Route;
 use yew::prelude::*;
 use yew_router::prelude::{use_route, Link};
 
+fn get_nav_classes(current_route: &Route, target_route: Route) -> Classes {
+    if *current_route == target_route {
+        classes!("nav-link", "active")
+    } else {
+        classes!("nav-link")
+    }
+}
+
 #[function_component(Sidebar)]
 pub fn sidebar() -> Html {
     let current_route = use_route::<Route>().expect("No Current route defined");
-
-    let home_classes = {
-        if current_route == Route::Home {
-            classes!("nav-link", "active")
-        } else {
-            classes!("nav-link")
-        }
-    };
-
-    let rustaceans_classes = {
-        if current_route == Route::Rustaceans {
-            classes!("nav-link", "active")
-        } else {
-            classes!("nav-link")
-        }
-    };
-
-    let crates_classes = {
-        if current_route == Route::Crates {
-            classes!("nav-link", "active")
-        } else {
-            classes!("nav-link")
-        }
-    };
 
     html! {
        <nav class="navbar navbar-light">
         <ul class="nav navbar-nav">
           <li class="nav-item">
-            <Link<Route> to={Route::Home} classes={home_classes}>
+            <Link<Route> to={Route::Home} classes={get_nav_classes(&current_route, Route::Home)}>
               {"HOME"}
             </Link<Route>>
           </li>
           <li class="nav-item">
-            <Link<Route> to={Route::Rustaceans} classes={rustaceans_classes}>
+            <Link<Route> to={Route::Rustaceans} classes={get_nav_classes(&current_route, Route::Rustaceans)}>
               {"Rustaceans"}
             </Link<Route>>
           </li>
           <li class="nav-item">
-            <Link<Route> to={Route::Crates} classes={crates_classes}>
+            <Link<Route> to={Route::Crates} classes={get_nav_classes(&current_route, Route::Crates)}>
               {"Crates"}
             </Link<Route>>
           </li>
